e9ffad62ee00972d7b2416d6b4896b476bef0a33
Jonathan Henrique <jhowjhoe@icloud.com>
2017-08-28 11:28:27 -0300

    update

--- top.php
+++ top.php
@@ -11,7 +11,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>CMS Lite | Dashboard</title>
+  <title>CMSLite | Dashboard</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="components/font-awesome/css/font-awesome.min.css">
