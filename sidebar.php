3056779b2e7bf8069596864b98a23613e183821d
Jonathan Henrique <jhowjhoe@icloud.com>
2017-10-18 10:11:57 -0200

    bugfix
    bugfix

--- sidebar.php
+++ sidebar.php
@@ -9,40 +9,40 @@
 
 ?>
 <aside class="main-sidebar">
-  <section class="sidebar">
-    <form action="#" method="get" class="sidebar-form">
-      <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Buscar...">
-        <span class="input-group-btn">
-          <button type="submit" name="search" id="search-btn" class="btn btn-flat">
-            <i class="fa fa-search"></i>
-          </button>
-        </span>
-      </div>
-    </form>
-    <ul class="sidebar-menu" data-widget="tree">
-      <li class="header">MENU PRINCIPAL</li>
-      <li>
-        <a href="index.php">
-          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-          <span class="pull-right-container">
-            <small class="label pull-right bg-green">novo</small>
-          </span>
-        </a>
-      </li>
-      <li class="treeview">
-        <a href="#">
-          <i class="fa fa-user"></i>
-          <span>Menus</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li><a href="pagina1.php"><i class="fa fa-circle-o"></i> Menu 1</a></li>
-          <li><a href="pagina2.php"><i class="fa fa-circle-o"></i> Menu 2</a></li>
-        </ul>
-      </li>
-    </ul>
-  </section>
+	<section class="sidebar">
+		<form action="#" method="get" class="sidebar-form">
+			<div class="input-group">
+				<input type="text" name="q" class="form-control" placeholder="Buscar...">
+				<span class="input-group-btn">
+					<button type="submit" name="search" id="search-btn" class="btn btn-flat">
+						<i class="fa fa-search"></i>
+					</button>
+				</span>
+			</div>
+		</form>
+		<ul class="sidebar-menu" data-widget="tree">
+			<li class="header">MENU PRINCIPAL</li>
+			<li>
+				<a href="index.php">
+					<i class="fa fa-dashboard"></i> <span>Dashboard</span>
+					<span class="pull-right-container">
+						<small class="label pull-right bg-green">home</small>
+					</span>
+				</a>
+			</li>
+			<li class="treeview">
+				<a href="#">
+					<i class="fa fa-user"></i>
+					<span>Menus</span>
+					<span class="pull-right-container">
+						<i class="fa fa-angle-left pull-right"></i>
+					</span>
+				</a>
+				<ul class="treeview-menu">
+					<li><a href="views/pagina1.php"><i class="fa fa-circle-o"></i> Menu 1</a></li>
+					<li><a href="views/pagina2.php"><i class="fa fa-circle-o"></i> Menu 2</a></li>
+				</ul>
+			</li>
+		</ul>
+	</section>
 </aside>
---
+++ views/pagina1.php
@@ -0,0 +1,27 @@
+<?php
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//    CMSLite Versão 1.1                    //
+//    Developer: Agencia 3WEB               //
+//    Site: agencia3web.com                 //
+//    GitHub: github.com/jhowbhz/CMSLite    //
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+require_once('../top.php'); 
+require_once('../header.php'); 
+require_once('../sidebar.php');
+?>
+
+<div class="content-wrapper">
+	<section class="content">
+		<div class="row">
+			<div class="col-md-3 col-sm-6 col-xs-12">
+				Conteudo PAGINA 1
+			</div>
+		</div>
+	</section>
+</div>
+
+<?php
+require_once ('../footer.php'); 
+
+?>
